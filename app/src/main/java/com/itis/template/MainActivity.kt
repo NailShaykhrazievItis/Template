a1f8d6d6e5de31f2282623984ee8d2c80e14a623
Nail <shaykhrazievnail@gmail.com>
2020-02-17 16:47:45 +0300

    Add open weather api integration

--- app/src/main/java/com/itis/template/MainActivity.kt
+++ app/src/main/java/com/itis/template/MainActivity.kt
@@ -2,12 +2,41 @@ package com.itis.template
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import com.itis.template.response.WeatherResponse
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.lang.Exception
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope by MainScope() {
+
+    private lateinit var service: WeatherService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        service = ApiFactory.weatherService
+
+        launch {
+            val response = withContext(Dispatchers.IO) {
+                service.weatherByName("Moscow")
+            }
+            Snackbar.make(
+                findViewById(android.R.id.content),
+                response.main.toString(),
+                Snackbar.LENGTH_INDEFINITE
+            ).show()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        coroutineContext.cancelChildren()
     }
 }
---
+++ app/src/main/java/com/itis/template/WeatherService.kt
@@ -0,0 +1,13 @@
+package com.itis.template
+
+import com.itis.template.response.WeatherResponse
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherService {
+
+    @GET("weather")
+    suspend fun weatherByName(@Query("q") name: String): WeatherResponse
+}
+
