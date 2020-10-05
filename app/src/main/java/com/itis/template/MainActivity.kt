25ff048b34f669fa49ab56d9574379568eb60322
Nail <shaykhrazievnail@gmail.com>
2020-10-06 01:27:01 +0300

    Add test fragments

--- app/src/main/java/com/itis/template/MainActivity.kt
+++ app/src/main/java/com/itis/template/MainActivity.kt
@@ -1,13 +1,26 @@
 package com.itis.template
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        LessonFragment.newInstance("1", "2").also {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragment_container, it, "TAG")
+                .commit()
+        }
+//        Example of using android.R.id.content
+//        Snackbar.make(findViewById(android.R.id.content), "HELLO", Snackbar.LENGTH_LONG).show()
+
+//        Example of adding view to layout from code
+//        LayoutInflater.from(this).also {
+//            it.inflate(R.layout.test_view, container, true)
+//        }
     }
+
 }
