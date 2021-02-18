f2f288a47b4f0ac629d174d470b645d05a5fe8cb
Nail <shaykhrazievnail@gmail.com>
2021-02-18 21:48:15 +0300

    Add api requests

--- app/src/main/java/com/itis/template/MainActivity.kt
+++ app/src/main/java/com/itis/template/MainActivity.kt
@@ -1,13 +1,34 @@
 package com.itis.template
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private val api = ApiFactory.weatherApi
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tv_hello.setOnClickListener {
+            onHelloClick()
+        }
+    }
+
+    private fun onHelloClick() {
+        lifecycleScope.launch {
+            api.getWeather("Kazan").run {
+                Snackbar.make(
+                    findViewById(android.R.id.content),
+                    "Шаhaр Температурасы: ${main.temp}",
+                    Snackbar.LENGTH_LONG
+                ).show()
+            }
+        }
     }
 }
